dea0f317927accce48d6256198d06c8602163bbc
Sofya Tarnalitskaya <tarnalitska@gmail.com>
2021-02-25 13:18:15 +0300

    feat: add code for typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!Array.isArray(array)){
+    return 0
+  } else if (!array.length){
+    return 0
+  } else {
+    return Math.min(...array)
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!Array.isArray(array)){
+    return 0
+  } else if (!array.length){
+    return 0
+  } else {
+    return Math.max(...array);
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!Array.isArray(array)){
+    return 0
+  } else if (!array.length){
+    return 0
+  }else {
+    return array.reduce((a, b) => a + b, 0) / array.length;
+  }
 }
